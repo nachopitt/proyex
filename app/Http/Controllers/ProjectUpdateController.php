10a095ac9f2e5f1b6b7d69646dc21af6d8e4455e
nachopitt <nachopitt@gmail.com>
2025-09-03 06:23:34 -0600

    feat(project): Scaffold ProjectUpdate resource
    This commit scaffolds the ProjectUpdate resource, including the model,
    controller, policy, factory, and form requests.
    
    php artisan make:model --all ProjectUpdate
    
    This provides the basic structure for managing project updates within
    the application.

---
+++ app/Http/Controllers/ProjectUpdateController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProjectUpdateRequest;
+use App\Http\Requests\UpdateProjectUpdateRequest;
+use App\Models\ProjectUpdate;
+
+class ProjectUpdateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProjectUpdateRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProjectUpdate $projectUpdate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProjectUpdate $projectUpdate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProjectUpdateRequest $request, ProjectUpdate $projectUpdate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProjectUpdate $projectUpdate)
+    {
+        //
+    }
+}
