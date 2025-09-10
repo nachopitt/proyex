c51a23c597bf737214c80b57da0844ab466a98a4
nachopitt <nachopitt@gmail.com>
2025-09-09 22:10:22 -0600

    refactor: Reorder Status enum cases
    Reordered `ON_HOLD` and `COMPLETED` cases in `app/Enums/Status.php` to
    reflect a more logical progression of statuses.  Updated associated
    integer values and `getLabel()` method accordingly.

--- app/Enums/Status.php
+++ app/Enums/Status.php
@@ -11,8 +11,8 @@ enum Status: int implements HasLabel
 
     case PLANNED = 1;
     case IN_PROGRESS = 2;
-    case COMPLETED = 3;
-    case ON_HOLD = 4;
+    case ON_HOLD = 3;
+    case COMPLETED = 4;
     case CANCELLED = 5;
 
     public function getLabel(): string
@@ -20,8 +20,8 @@ enum Status: int implements HasLabel
         return match ($this) {
             self::PLANNED => 'Planned',
             self::IN_PROGRESS => 'In progress',
-            self::COMPLETED => 'Completed',
             self::ON_HOLD => 'On hold',
+            self::COMPLETED => 'Completed',
             self::CANCELLED => 'Cancelled',
         };
     }
